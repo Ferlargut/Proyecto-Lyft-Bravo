8a967bab45b3d4ac42b6ca12848a7cc96aa8d7f8
nancyzj <ancizj@hotmail.com>
2017-04-27 15:15:10 -0500

    mejoras en el formulario

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
 //parte del formulario nancy
 var numero = document.getElementById('numero');
 numero.addEventListener("click",aparecer);
+//variable para el placeholder
+var fN = document.getElementsByClassName('fN');
+
+for(var i=0; i< fN.length; i++){
+	fN[i].addEventListener('click',pequenoPlaceholder);
+}
+
+//hacer una funcion para el fn
+function pequenoPlaceholder(){
+	alert("hola");
+	
+}
 
 //funcion
 function aparecer(){
 	var formularioDatos = document.getElementById('formularioDatos');
 	if(formularioDatos.style.display == "none"){
-		formularioDatos.style.display = "block";
+    formularioDatos.style.display = "block";		
 	}else{	
 		formularioDatos.style.display = "none";
 	}
 }
+//fin de nancy
